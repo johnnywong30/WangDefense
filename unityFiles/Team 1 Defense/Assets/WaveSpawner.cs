55c964193e382c138227fa594e98c357f932cd67
Johnny Wong <johnny.wong2324@gmail.com>
2020-10-20 18:07:37 -0400

    started wavespawner, need to make it scalable

---
+++ unityFiles/Team 1 Defense/Assets/WaveSpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+    
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f; // make this longer later... 5f for demo purposes
+    private float countdown = 2f;       // 2 seconds till first wave...
+    public Text waveCount;
+
+    private int wave = 1;
+    private int maxWave = 5;            // change this every level depending on difficulty goals... figure out how to set this at runtime...
+    
+
+    void Start () {
+        waveCount.text = "Get ready...";
+    }
+
+    void Update() {
+        if (countdown <= 0f) {
+            if (wave < maxWave + 1){
+                StartCoroutine(SpawnWave());
+                countdown = timeBetweenWaves;
+            }
+        }
+        countdown -= Time.deltaTime;    // last time since last frame
+        
+        
+    }
+
+    // eventually adjust this to determine how we want waves to work...
+    IEnumerator SpawnWave () {
+        // Debug.Log("Wave Incoming!");
+        waveCount.text = "Wave " + wave.ToString();
+        for (int i = 0; i < wave; i++) {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f); // wait like half a second for each enemy per wave
+        }
+        wave++;
+        
+    }
+
+    void SpawnEnemy() {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+
+}
