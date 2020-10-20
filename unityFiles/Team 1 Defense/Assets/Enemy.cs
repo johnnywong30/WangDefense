b50d17fef69d8c73e551165277704526346ea067
Johnny Wong <johnny.wong2324@gmail.com>
2020-10-19 20:56:52 -0400

    made enemy be able to move

---
+++ unityFiles/Team 1 Defense/Assets/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f; // arbitrary speed
+
+    private Transform target;
+    private int waypointIndex = 0; // waypoint target index
+
+    void Start () {
+        target = Waypoints.points[0];
+    }
+
+    void Update() {
+        Vector3 dir = target.position - transform.position;
+        // print(dir);
+        // normalized vector * constant speed * times passed since last frame
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World); 
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f) {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint() {
+        if (waypointIndex >= Waypoints.points.Length - 1){
+            Destroy(gameObject); // reached the end, clear this object...
+            return;
+        }
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+
+}
