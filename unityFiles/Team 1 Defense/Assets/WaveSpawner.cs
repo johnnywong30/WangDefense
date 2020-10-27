eab44566f99cdd7d6a4d9eac39fd88a682bbb860
Johnny Wong <johnny.wong2324@gmail.com>
2020-10-27 12:25:12 -0400

    made wavespawner scalable

--- unityFiles/Team 1 Defense/Assets/WaveSpawner.cs
+++ unityFiles/Team 1 Defense/Assets/WaveSpawner.cs
@@ -8,7 +8,7 @@ public class WaveSpawner : MonoBehaviour {
 
     public Transform spawnPoint;
 
-    public float timeBetweenWaves = 5f; // make this longer later... 5f for demo purposes
+    public float timeBetweenWaves; // make this longer later... 5f for demo purposes
     private float countdown = 2f;       // 2 seconds till first wave...
     public Text waveCount;
 
