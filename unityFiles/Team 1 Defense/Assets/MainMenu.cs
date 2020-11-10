b2b3ce0b61b7676076f89063de12ad5957c15023
iFallenDawn <jwang203@stevens.edu>
2020-11-10 00:00:58 -0500

    main menu functionality and scene added

---
+++ unityFiles/Team 1 Defense/Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string level1 = "Level 1";
+    public void Play()
+    {
+        Debug.Log("Play");
+        SceneManager.LoadScene(level1);
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Settings");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exiting...");
+        Application.Quit();
+    }
+}
